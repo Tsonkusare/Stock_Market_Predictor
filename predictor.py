11d30f530dfe514e61692d79e21700a178f38b22
Tsonkusare <tarangpsonkusare@gmail.com>
2025-06-22 02:27:36 -0400

    After youtube video

--- predictor.py
+++ predictor.py
@@ -1,7 +1,83 @@
-import finnhub
-import os
-from dotenv import load_dotenv
+import numpy as np
+import pandas as pd
+import yfinance as yf
+from datetime import date
+from keras.models import load_model
+import streamlit as st
+from dateutil.relativedelta import relativedelta
+from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 
-load_dotenv() #loading the secret API file
-API_KEY = os.getenv("API_KEY") #Set API_KEY = to your api key
-finnhub_client = finnhub.Client(api_key=API_KEY)
+model = load_model('Stock Prediction Model.keras')
+
+st.header('Stock Market Predictor')
+
+stock = st.text_input('Enter Stock Symbol', 'GOOG')
+end = date.today()
+start = end - relativedelta(years=10)
+
+data = yf.download(stock, start, end)
+
+st.subheader('Stock Data')
+st.write(data)
+
+
+data_train = pd.DataFrame(data.Close[0: int(len(data)*.80)])
+data_test = pd.DataFrame(data.Close[int(len(data)*.80):])
+
+scaler = MinMaxScaler(feature_range=(0,1))
+
+pas_100_days = data_train.tail(100)
+data_test = pd.concat([pas_100_days, data_test], ignore_index = True)
+data_test_scale = scaler.fit_transform(data_test)
+
+st.subheader('Price vs MA50')
+ma_50_days = data.Close.rolling(50).mean()
+fig1 = plt.figure(figsize= (8,6))
+plt.plot(ma_50_days, 'r')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig1)
+
+st.subheader('Price vs MA50 vs MA100')
+ma_100_days = data.Close.rolling(100).mean()
+fig2 = plt.figure(figsize= (8,6))
+plt.plot(ma_50_days, 'r')
+plt.plot(ma_100_days, 'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig2)
+
+st.subheader('Price vs MA100 vs MA200')
+ma_200_days = data.Close.rolling(200).mean()
+fig3 = plt.figure(figsize= (8,6))
+plt.plot(ma_200_days, 'y')
+plt.plot(ma_100_days, 'b')
+plt.plot(data.Close, 'g')
+plt.show()
+st.pyplot(fig3)
+
+x = []
+y = []
+
+for i in range(100, data_test_scale.shape[0]):
+    x.append(data_test_scale[i-100: i])
+    y.append(data_test_scale[i, 0])
+    
+x, y = np.array(x), np.array(y)
+
+predict = model.predict(x)
+
+scale = 1/scaler.scale_
+
+predict = predict * scale
+y = y* scale
+
+st.subheader('Original Price vs Predicted Price')
+fig3 = plt.figure(figsize= (8,6))
+plt.plot(predict, 'r', label = 'Original Price')
+plt.plot(y, 'b', label = 'Predicted Price')
+plt.xlabel('Time') 
+plt.ylabel('Price') 
+plt.show()
+st.pyplot(fig3)
